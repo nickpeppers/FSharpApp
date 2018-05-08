21d5708784e9c829420185c5ee4a31c8238cddef
nickpeppers <nicholas.peppers778@topper.wku.edu>
2018-05-08 10:38:39 -0400

    Added App.xaml file

--- FSharpApp/App.xaml.fs
+++ FSharpApp/App.xaml.fs
@@ -1,8 +1,10 @@
-namespace FSharpApp
+﻿namespace FSharpApp
 
 open Xamarin.Forms
+open Xamarin.Forms.Xaml
 
 type App() =
     inherit Application()
+    let _ = base.LoadFromXaml(typeof<App>)
 
     do base.MainPage <- NavigationPage(MainPage())
