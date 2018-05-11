8732d933f3377f14cf2c28af0304e14d848d69a5
nickpeppers <nicholas.peppers778@topper.wku.edu>
2018-05-11 19:36:56 -0400

    Added methods to test F# usage

--- FSharpApp/Pages/MainPage.xaml.fs
+++ FSharpApp/Pages/MainPage.xaml.fs
@@ -1,6 +1,7 @@
 ﻿namespace FSharpApp
 
 open System
+open System.Linq;
 open Xamarin.Forms
 open Xamarin.Forms.Xaml
 
@@ -9,6 +10,18 @@ type MainPage() =
     let _ = base.LoadFromXaml(typeof<MainPage>)
 
     let _editor = base.FindByName<Editor>("SpeechEditor")
+    let _outputLabel = base.FindByName<Label>("OutputLabel")
+
+    let Add x y = x + y
+    let Subtract x y = x - y
+    let Multiply x y = x * y
+
+    member this.OnRunClicked (sender : Object, args : EventArgs) =
+        let text = _editor.Text
+        let firstNumber = text.Split(' ').FirstOrDefault() |> int
+        let secondNumber = text.Split(' ').LastOrDefault() |> int
+        let newText = Add firstNumber secondNumber 
+        do _outputLabel.Text <- newText.ToString()
 
     member this.OnSpeakClicked (sender : Object, args : EventArgs ) =
         do DependencyService.Get<ITextToSpeech>().Speak(_editor.Text)
