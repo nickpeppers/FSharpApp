2508919592e22f89b3c8469610089a62cd60b731
Nick Peppers <nicholas.peppers778@topper.wku.edu>
2019-04-20 15:49:01 -0400

    Updated Packages, added NewPage and NewContentView for template creation

---
+++ FSharpApp/Controls/NewContentView.xaml.fs
@@ -0,0 +1,18 @@
+﻿namespace FSharpApp.Controls
+
+open System
+open Xamarin.Forms
+open Xamarin.Forms.Xaml
+
+type NewContentView() =
+    inherit ContentView()
+    let _ = base.LoadFromXaml(typeof<NewContentView>)
+
+    let _label = base.FindByName<Label>("_label")
+
+    let Add x y = x + y
+
+    member this.OnButtonClicked (sender : Object, args : EventArgs) =
+        let textValue = _label.Text |> int
+        let newText = Add textValue 1
+        do _label.Text <- newText.ToString()
---
+++ FSharpApp/Pages/NewPage.xaml.fs
@@ -0,0 +1,18 @@
+﻿namespace FSharpApp
+
+open System
+open Xamarin.Forms
+open Xamarin.Forms.Xaml
+
+type NewPage() =
+    inherit ContentPage()
+    let _ = base.LoadFromXaml(typeof<NewPage>)
+
+    let _label = base.FindByName<Label>("_label")
+
+    let Add x y = x + y
+
+    member this.OnButtonClicked (sender : Object, args : EventArgs) =
+        let textValue = _label.Text |> int
+        let newText = Add textValue 1
+        do _label.Text <- newText.ToString()
